afa0b1ed70f1ae309578b2f3eed0b8523622d991
Kevin Breid <kevinbreid@yahoo.com>
2023-10-18 11:43:52 +0200

    Practicing class inheritance in Python

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self,first_name, last_name, knowledge = None):
+        super().__init__(first_name, last_name)
+        if knowledge == None:
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, string):
+        self.knowledge.append(string)
